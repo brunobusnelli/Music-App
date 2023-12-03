28984deda156df2d8171b82e37b5e1c999f54a73
BrunoBusnelli <bbusnelli1@gmail.com>
2023-12-03 00:35:25 -0300

    cierre de sesion

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,36 +1,43 @@
-import React, { useEffect, useState } from 'react'
-import './sidebar.css'
-import SidebarButton from './SidebarButton'
-import { MdDashboard } from "react-icons/md";
-import { MdOutlineLibraryMusic } from "react-icons/md";
-import { FaPlay } from "react-icons/fa";
-import { FaSignOutAlt } from "react-icons/fa";
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import './sidebar.css';
+import SidebarButton from './SidebarButton';
+import { MdDashboard } from 'react-icons/md';
+import { MdOutlineLibraryMusic } from 'react-icons/md';
+import { FaPlay } from 'react-icons/fa';
+import { FaSignOutAlt } from 'react-icons/fa';
 import apiClient from '../../spotify';
 
-
-
 const Sidebar = () => {
-  const [user, setUser] = useState([])
-  useEffect(() => {
-     apiClient.get("me").then(response =>{
-      setUser(response.data)
-    } )
-  }, [])
-  
-  return (
-    <div className='sidebar-container'>
-        <div className='user-container flex'>
-        <img src={user?.images?.[0]?.url}  alt="profile img" className='profile-img' />
-        <p className='user-name'>{user?.display_name}</p>
-        </div>
-        <div>
-          <SidebarButton title="Feed" to="/feed" icon={<MdDashboard />} />
-          <SidebarButton title="Library" to="/" icon={<MdOutlineLibraryMusic />} />
-          <SidebarButton title="Player"  to="/player" icon={<FaPlay />} />
-        </div>
-        <SidebarButton title="Sign out" to="" icon={<FaSignOutAlt />} />
-    </div>
-  )
-}
+  const [user, setUser] = useState([]);
+  const navigate = useNavigate();
 
-export default Sidebar
+  useEffect(() => {
+    apiClient.get('me').then((response) => {
+      setUser(response.data);
+    });
+  }, []);
+
+  const handleSignOut = () => {
+    localStorage.removeItem('token');
+    navigate('/login');
+    window.location.reload();
+  };
+
+  return (
+    <div className="sidebar-container">
+      <div className="user-container flex">
+        <img src={user?.images?.[0]?.url} alt="profile img" className="profile-img" />
+        <p className="user-name">{user?.display_name}</p>
+      </div>
+      <div>
+        <SidebarButton title="Feed" to="/feed" icon={<MdDashboard />} />
+        <SidebarButton title="Library" to="/" icon={<MdOutlineLibraryMusic />} />
+        <SidebarButton title="Player" to="/player" icon={<FaPlay />} />
+      </div>
+      <SidebarButton title="Sign out" onClick={handleSignOut} icon={<FaSignOutAlt />} />
+    </div>
+  );
+};
+
+export default Sidebar;
--- src/spotify.js
+++ src/spotify.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const authEndpoint = "https://accounts.spotify.com/authorize?"
 const clientId = "474a5c64b382441a9e5a8bf9b3a6375d"
-const redirectUri = "http://localhost:5173"
+const redirectUri = "https://classy-capybara-f78b1e.netlify.app/"
 const scopes = ["user-library-read","playlist-read-private"]
 
 export const loginEndpoint = `${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
